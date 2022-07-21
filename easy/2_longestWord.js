d645fc82c5a687b0ca9370ae1d434e8924e0edfd
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-07-21 17:46:47 +0300

    commited longestWord

---
+++ easy/2_longestWord.js
@@ -0,0 +1,17 @@
+export const longestWord =(sen) =>{
+    //1-Noktalama işaretlerini kaldırma. Harf ya ada boşluk değilse yerine boşluk koyma ve 
+    //2-Cümleyi boşluklardan bölerek, arr oluşturma
+    const arr =sen.replace(/[^a-zA-Z ]/g, "").split(" ")
+
+    //3-Arrayi, karekter sayısına göre sıralamak
+    arr.sort((a,b)=>{return b.length-a.length})
+    //4-İlk öğeyi almak
+    return arr[0]
+
+
+}
+
+
+/*
+Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+*/
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import { userValidation } from "./easy/1_userValidation.js"
+//1_userValidation
+// import { userValidation } from "./easy/1_userValidation.js"
+//console.log(userValidation("u__hello_world123")) 
 
-
-console.log(userValidation("u__hello_world123"))
+//2_longestWord
+import { longestWord } from "./easy/2_longestWord.js";
+console.log(longestWord("fun&!! time"))
