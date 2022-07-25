34410895c318e77412d03de0ee7e518679f2c661
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-07-25 16:53:53 +0300

    simple adding commited

---
+++ easy/8_simpleAdding.js
@@ -0,0 +1,12 @@
+const simpleAdding= (num) =>{
+    result = 0
+    for(i=1; i<=num; i++){
+        result += i
+    }
+    return result
+}
+
+console.log(simpleAdding(15))
+
+
+// Verilen number değerine kadar olan sayıların toplamını veren algoritma. 
