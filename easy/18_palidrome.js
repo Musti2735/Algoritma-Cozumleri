9b9b34dabfffe6687c688cc1f55f2d30accba02f
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-07-28 17:16:50 +0300

    commited

--- easy/18_palidrome.js
+++ easy/18_palidrome.js
@@ -1,12 +1,15 @@
 const palidrome = (str)=>{
-    let newStr = str.split("").reverse().join("")
-    return str === newStr ? true : false;
+
+    let newStr = str.replace(/ /g, "").toLowerCase()
+    
+    return newStr.split("").reverse().join("") === newStr ? true :false
+    
 }
-console.log(palidrome("racecar"))
+console.log(palidrome("racE car"))
 
 
 /*
 Verilen string ifadenin tersten okunuşunun da aynı olması halinde palidrome denilir. Bir kelimenin palidrome olup olmadığını bulan algoritma.
-string ifadeyi split ile arraye ve reverse ile terse çevirdik. join ile tekrar string yaptığımı ifadeyi ilk str ile karşılaştırdık.
+string ifadeyi öncelikle regexp kullanarak aradaki boşluk ifadeyi kaldırdık ve tüm harfleri küçük harfe dönüştürdük. split ile arraye ve reverse ile terse çevirdik. join ile tekrar string yaptığımı ifadeyi ilk str ile karşılaştırdık.
 
 */
