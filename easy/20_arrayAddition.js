ecae7d52afc692ebbad81cfbb63be37cdca25c51
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-07-29 14:02:26 +0300

    array adition commited

---
+++ easy/20_arrayAddition.js
@@ -0,0 +1,17 @@
+const arrayAddition =(arr) =>{
+    let maxNum = Math.max(...arr)
+    let total = 0
+    for(let i=0; i<arr.length; i++){
+      total += arr[i]
+    }
+  return total/2 == maxNum ? true : false;
+}
+
+console.log(arrayAddition([5,2,23,6,3,7]))
+
+
+/*
+Verilen liste içindeki en büyük eleman ile listenin diğer elemanlarının toplamının eşit olup olmadığını karşılaştıran algoritma.
+Max değeri yine Math.max() metodu ile bulduktan sonra bütün array içerisindeki elemanları topladık ve ikiye böldük. Eğer bölüm max değere eşitse true aksi durumlarda ise yine false döndük.
+
+*/
