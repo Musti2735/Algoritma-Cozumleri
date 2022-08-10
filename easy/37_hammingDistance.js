94b6c8ef405f1386503b8df173006210661b929d
Mustafa Turk <av.mustafaturk@hotmail.com>
2022-08-10 16:59:54 +0300

    hamming distance commited

---
+++ easy/37_hammingDistance.js
@@ -0,0 +1,16 @@
+const hammingDistance =(p1, p2) => {
+    count = 0
+    for (let i = 0; i<=p1.length; i++){
+        if(p1[i] != p2[i]) count +=1
+    }
+    return count
+
+}
+
+console.log(hammingDistance("karolin", "kathrin"))
+
+
+/* Soruda, verilen iki kelimenin kaç farklı harfinin olduğu sorulmuştur. 
+Çözüm olarak bir sayaç tanımladık ve for döngüsü ile iki kelimenin her bir harfini birbiri ile karşılaştırdık. Eğer harfler farklıysa count değerini 1 artırdık.
+
+*/
